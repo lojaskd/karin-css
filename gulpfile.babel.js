0c07c55f0f0d8edd5d90be26b975c657ceb228c5
Fernando Moreira <nandomoreira.me@gmail.com>
2017-03-21 16:15:53 -0300

    Removed dependency material icons

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,10 @@
 import Gulp from 'gulp';
 import Plugins from 'gulp-load-plugins';
 
-import icons from './tasks/icons';
 import stylesheets from './tasks/stylesheets';
 import version from './tasks/version';
 
-Gulp.task('watch', ['stylesheets', 'icons'], () => {
+Gulp.task('watch', ['stylesheets'], () => {
   Gulp.watch(`./sass/**/*.sass`, ['stylesheets']);
 });
 
